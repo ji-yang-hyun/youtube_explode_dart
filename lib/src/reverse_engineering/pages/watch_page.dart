e1f9adc64b76913c69e4d0961b60edbcd8c0d997
ji-yang-hyun <127380736+ji-yang-hyun@users.noreply.github.com>
2026-02-06 20:04:34 +0900

    Update watch_page.dart

--- lib/src/reverse_engineering/pages/watch_page.dart
+++ lib/src/reverse_engineering/pages/watch_page.dart
@@ -172,6 +172,7 @@ class WatchPage extends YoutubePage<WatchPageInitialData> {
       // final cookies = {};
 
       print(cookies);
+      print(cookies.runtimeType);
       
       final result = WatchPage.parse(req.body, VideoId(videoId), cookies);
 
